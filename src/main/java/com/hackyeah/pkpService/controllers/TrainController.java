d124cd8304320d2671b414bb8e35ade405cb7354
Giga <giga.chalauri@gmail.com>
2018-11-24 18:09:46 +0100

    url

--- src/main/java/com/hackyeah/pkpService/controllers/TrainController.java
+++ src/main/java/com/hackyeah/pkpService/controllers/TrainController.java
@@ -1,14 +1,17 @@
 package com.hackyeah.pkpService.controllers;
 
 import com.hackyeah.pkpService.entities.Train;
-import com.hackyeah.pkpService.entities.WagonType;
 import com.hackyeah.pkpService.services.TrainService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/train")
 public class TrainController {
 
     private TrainService trainService;
