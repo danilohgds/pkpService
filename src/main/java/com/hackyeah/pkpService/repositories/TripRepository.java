9a998f5ddd8f8fd9fa4335f48cefca5dadeaff34
Filip <Filip_Hartman@epam.com>
2018-11-25 00:18:38 +0100

    add repository for trip

--- src/main/java/com/hackyeah/pkpService/repositories/TripRepository.java
+++ src/main/java/com/hackyeah/pkpService/repositories/TripRepository.java
@@ -4,6 +4,11 @@ import com.hackyeah.pkpService.relationships.Trip;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.Instant;
+import java.util.List;
+
 @Repository
 public interface TripRepository extends CrudRepository<Trip, String> {
+    List<Trip> findByStartCityAndEndCity(String startCityId, String endCityId);
+    List<Trip> findByStartTime(Instant startTime);
 }
