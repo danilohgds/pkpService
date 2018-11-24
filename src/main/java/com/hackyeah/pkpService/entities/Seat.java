73033853cdded2949581278b5954fcfdf1ba3b0b
Filip <Filip_Hartman@epam.com>
2018-11-24 13:57:40 +0100

    Creating seat and wagon entities for api

--- src/main/java/com/hackyeah/pkpService/entities/Seat.java
+++ src/main/java/com/hackyeah/pkpService/entities/Seat.java
@@ -1,7 +1,27 @@
 package com.hackyeah.pkpService.entities;
 
-import javax.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
 
+import javax.persistence.*;
+
+@Getter
+@Setter
 @Entity
 public class Seat {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "seat_id")
+    private long seatId;
+    @Column(name = "seat_number")
+    private int number;
+    @ManyToOne
+    private Wagon wagon;
+
+    public Seat(long seatId, int number, Wagon wagon) {
+        this.seatId = seatId;
+        this.number = number;
+        this.wagon = wagon;
+    }
 }
--- src/main/java/com/hackyeah/pkpService/entities/Wagon.java
+++ src/main/java/com/hackyeah/pkpService/entities/Wagon.java
@@ -1,8 +1,31 @@
 package com.hackyeah.pkpService.entities;
 
-import javax.persistence.Entity;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import javax.persistence.*;
+import java.util.Collection;
+
+@Getter
+@Setter
+@NoArgsConstructor
 @Entity
 public class Wagon {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private long id;
+    @Column(name = "wagon_number")
+    private int wagonNumber;
+    @Column(name = "number_of_places")
+    private int numberOfPlaces;
+    @OneToMany(mappedBy = "wagon")
+    private Collection<Seat> seats;
 
+    public Wagon(long id, int wagonNumber, int numberOfPlaces, Collection<Seat> seats) {
+        this.id = id;
+        this.wagonNumber = wagonNumber;
+        this.numberOfPlaces = numberOfPlaces;
+        this.seats = seats;
+    }
 }
