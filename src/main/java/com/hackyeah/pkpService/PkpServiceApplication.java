7283b89b99e965723d32372fdd3a728c8cbba502
Danilo_Silva@epam.com <Danilo_Silva@epam.com>
2018-11-24 23:12:58 +0100

    changes to health and login

--- src/main/java/com/hackyeah/pkpService/PkpServiceApplication.java
+++ src/main/java/com/hackyeah/pkpService/PkpServiceApplication.java
@@ -4,8 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 
+@SuppressWarnings("deprecation")
 @SpringBootApplication
 @EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class PkpServiceApplication {
@@ -13,4 +18,14 @@ public class PkpServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PkpServiceApplication.class, args);
 	}
+	
+	@Bean
+	    public WebMvcConfigurer corsConfigurer() {
+	        return new WebMvcConfigurerAdapter() {
+	            @Override
+	            public void addCorsMappings(CorsRegistry registry) {
+	                registry.addMapping("*").allowedOrigins("*");
+	            }
+	        };
+	    }
 }
--- src/main/java/com/hackyeah/pkpService/controllers/HealthController.java
+++ src/main/java/com/hackyeah/pkpService/controllers/HealthController.java
@@ -1,7 +1,10 @@
 package com.hackyeah.pkpService.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -13,5 +16,18 @@ public class HealthController {
 		return "Backend is up and Running";
 
 	}
+	
+	
+	
+	@CrossOrigin(origins = "*")
+	@PostMapping("/login")
+	@ResponseBody
+	public Boolean getLogin(@RequestBody(required=true) String password) {
+		if(password.equalsIgnoreCase("epam2018")) {
+			return true;
+		}	
+		System.out.println(password);	
+		return false;
 
+	}
 }
