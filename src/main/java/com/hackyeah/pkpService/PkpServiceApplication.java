88699550bd38846d7acb6eee493cb69e37e03da3
Danilo_Silva@epam.com <Danilo_Silva@epam.com>
2018-11-24 13:20:49 +0100

    initial repo

---
+++ src/main/java/com/hackyeah/pkpService/PkpServiceApplication.java
@@ -0,0 +1,12 @@
+package com.hackyeah.pkpService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PkpServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PkpServiceApplication.class, args);
+	}
+}
