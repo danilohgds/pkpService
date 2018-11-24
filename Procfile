9b71390bfb0d6063f4383208b615d83c3d7bb634
Danilo_Silva@epam.com <Danilo_Silva@epam.com>
2018-11-24 16:15:21 +0100

    added files for heroku support

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java -jar target/pkpService-0.0.1-SNAPSHOT.jar
---
+++ src/main/java/com/hackyeah/pkpService/controllers/HealthController.java
@@ -0,0 +1,16 @@
+package com.hackyeah.pkpService.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HealthController {
+
+	@GetMapping("/health")
+	@ResponseBody
+	public String getHealth(){
+		return "Backend is up and Running";
+		
+	}
+}
