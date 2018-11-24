bb263b9c7c5111b126c8b534405c992cc96da942
Danilo_Silva@epam.com <Danilo_Silva@epam.com>
2018-11-24 19:45:04 +0100

    .

--- src/main/java/com/hackyeah/pkpService/controllers/HealthController.java
+++ src/main/java/com/hackyeah/pkpService/controllers/HealthController.java
@@ -2,6 +2,7 @@ package com.hackyeah.pkpService.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -12,5 +13,10 @@ public class HealthController {
 	public String getHealth(){
 		return "Backend is up and Running";
 		
+	}
+	
+	@RequestMapping("/hello")
+	String hello() {
+	  	    return "hello";
 	}
 }
