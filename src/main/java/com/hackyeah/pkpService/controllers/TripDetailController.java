c94e086caea8c71b8e53a666e34a02f69fc03203
Filip <Filip_Hartman@epam.com>
2018-11-25 02:37:35 +0100

    add trip detail controller

---
+++ src/main/java/com/hackyeah/pkpService/controllers/TripDetailController.java
@@ -0,0 +1,40 @@
+package com.hackyeah.pkpService.controllers;
+
+import com.hackyeah.pkpService.entities.TripDetail;
+import com.hackyeah.pkpService.services.TripDetailService;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.Instant;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/trip-details")
+public class TripDetailController {
+
+    private TripDetailService tripDetailService;
+
+    public TripDetailController(TripDetailService tripDetailService) {
+        this.tripDetailService = tripDetailService;
+    }
+
+    @RequestMapping(value = "byTripId", method = RequestMethod.GET)
+    public Optional<TripDetail> findByTripId(@RequestParam String tripId) {
+        return tripDetailService.findByTripId(tripId);
+    }
+
+    @RequestMapping(value = "byTrainId", method = RequestMethod.GET)
+    public List<TripDetail> findAllByTrinId(@RequestParam String trainId) {
+        return tripDetailService.findAlllByTrainId(trainId);
+    }
+
+    @RequestMapping(value = {"byStartDate", "byEndDate"}, method = RequestMethod.GET)
+    public List<TripDetail> findByStartAndEndDate(Instant strartDate, Instant endDate) {
+        return tripDetailService.findAllByStartAndEndTime(strartDate, endDate);
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public TripDetail create(@RequestBody TripDetail tripDetail) {
+        return tripDetailService.create(tripDetail);
+    }
+}
