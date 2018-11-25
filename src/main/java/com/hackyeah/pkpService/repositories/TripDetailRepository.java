9018ed711ec9857756bdc9553a8770179489bd0c
Filip <Filip_Hartman@epam.com>
2018-11-25 02:07:14 +0100

    Add trip detail repository

---
+++ src/main/java/com/hackyeah/pkpService/repositories/TripDetailRepository.java
@@ -0,0 +1,16 @@
+package com.hackyeah.pkpService.repositories;
+
+import com.hackyeah.pkpService.entities.TripDetail;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.Instant;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface TripDetailRepository extends MongoRepository<TripDetail, String> {
+    Optional<TripDetail> findByTripId(String tripId);
+    List<TripDetail> findAllByTrainId(String trainId);
+    List<TripDetail> findAllByStrartAndEndTime(Instant startTime, Instant endTime);
+}
